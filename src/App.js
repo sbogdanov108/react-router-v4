d657e5bd0d71d320808cfb78280f3a79059ed8c1
Sergey Bogdanov <bgrodthai@gmail.com>
2017-04-08 14:34:20 +0300

    Add regexp demo

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 const App = () => (
   <Router>
     <div>
-      {/* route props destruction */}
-      <Route path='/:page?/:subpage?' render={({match}) => (
+      <Route path='/:a(\d{2}-\d{2}-\d{4}):b(\.[a-z]+)' render={({match}) => (
         <h1>
-          PAGE: {match.params.page || 'Home'}<br/> SUBPAGE: {match.params.subpage}
+          param A: {match.params.a} <br/> param B: {match.params.b}
         </h1>
-      )} />
+      )}/>
     </div>
   </Router>
 );
