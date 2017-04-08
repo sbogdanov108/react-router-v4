6be2b2f9fa1d126c15d8b06143758630d34bb0d2
Sergey Bogdanov <bgrodthai@gmail.com>
2017-04-08 15:57:14 +0300

    Add router & redux

--- src/App.js
+++ src/App.js
@@ -1,56 +1,45 @@
 import React from 'react';
-import {BrowserRouter, HashRouter, MemoryRouter, StaticRouter, Route, Link} from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
-import './App.css';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
 
-const LinksRoutes = () => (
-  <div>
-    <Link to='/'>Home</Link>
-    <Link to='/about'>About</Link>
+import createHistory from 'history/createBrowserHistory';
+import { Route } from 'react-router';
 
-    <Route exact path='/' render={() => <h1>Home</h1>} />
-    <Route path='/about' render={() => <h1>About</h1>} />
-  </div>
+import { ConnectedRouter, routerReducer, routerMiddleware, push } from 'react-router-redux';
+
+import reducers from './reducers'; // То место, где вы храните редукторы
+
+// Создание выбранной вами истории для браузера
+const history = createHistory();
+
+// Создаем middleware для перехвата и отправки действий навигации
+const middleware = routerMiddleware(history);
+
+// Добавляем редукторы в хранилище по ключу router
+// Применяем наше middleware для навигации
+const store = createStore(
+  combineReducers({
+    ...reducers,
+    router: routerReducer
+  }),
+  applyMiddleware(middleware)
 );
 
-// Принудительное обновление страницы
-const forceRefresh = () => {
-  console.log(new Date());
-  return false;
-};
+// Теперь мы можем отправлять действия навигации из любого места!
+// store.dispatch(push('/foo'))
 
-const BrowserRouterApp = () => (
-  // HTML5 History
-  <BrowserRouter forceRefresh={forceRefresh()}>
-    <LinksRoutes />
-  </BrowserRouter>
+ReactDOM.render(
+  <Provider store={store}>
+    {/* ConnectedRouter будет использовать store из Provider автоматически */}
+    <ConnectedRouter history={history}>
+      <div>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/topics" component={Topics}/>
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('root')
 );
-
-const HashRouterApp = () => (
-  // Hash History
-  <HashRouter hashType='slash'>
-    <LinksRoutes />
-  </HashRouter>
-);
-
-const MemoryRouterApp = () => (
-  // Memory History - for testing purpose
-  <MemoryRouter
-    initialEntries={['/', '/about']}
-    initialIndex={1}
-  >
-    <LinksRoutes />
-  </MemoryRouter>
-);
-
-const StaticRouterApp = () => (
-  // Static - for server-side rendering
-  <StaticRouter location='/about' context={{}}>
-    <LinksRoutes />
-  </StaticRouter>
-);
-
-//export default StaticRouterApp;
-//export default MemoryRouterApp;
-export default HashRouterApp;
-//export default BrowserRouterApp;
