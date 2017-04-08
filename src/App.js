d017a522ef422ca3274f72a75e7931288d6535df
Sergey Bogdanov <bgrodthai@gmail.com>
2017-04-08 14:23:40 +0300

    Add extract params

--- src/App.js
+++ src/App.js
@@ -1,41 +1,15 @@
 import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  NavLink
-} from 'react-router-dom';
-
-import './App.css';
-
-const Home = () => <h1>Home</h1>;
-
-const isActiveFunc = (match, location) => {
-  return match;
-};
-
-const Links = () => (
-  // Разные способы задания активного стиля
-  <nav>
-    <NavLink exact activeClassName='active' to="/">Home</NavLink>
-
-    <NavLink activeStyle={{color: 'green'}} to='/about'>About</NavLink>
-
-    {/* Другой вариант задания ссылки */}
-    {/* <Link to={{pathname: '/about'}}>About</Link> */}
-
-    <NavLink isActive={isActiveFunc} activeClassName='active' to="/contact">Contact</NavLink>
-  </nav>
-);
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
 const App = () => (
   <Router>
     <div>
-      <Links/>
-
-      <Route exact path='/' component={Home}/>
-      <Route path='/about' render={() => <h1>About</h1>}/>
-      <Route path='/contact' render={() => <h1>Contact</h1>}/>
+      {/* route props destruction */}
+      <Route path='/:page?/:subpage?' render={({match}) => (
+        <h1>
+          PAGE: {match.params.page || 'Home'}<br/> SUBPAGE: {match.params.subpage}
+        </h1>
+      )} />
     </div>
   </Router>
 );
