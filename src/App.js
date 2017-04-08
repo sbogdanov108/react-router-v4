315a2c037f58e2d63bfc821e84cc5e13cc2340d0
Sergey Bogdanov <bgrodthai@gmail.com>
2017-04-08 13:19:30 +0300

    Add simple routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,43 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
+
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
-}
+const Home = () => <h1>Home</h1>;
+
+const isActiveFunc = (match, location) => {
+  return match;
+};
+
+const Links = () => (
+  // Разные способы задания активного стиля
+  <nav>
+    <NavLink exact activeClassName='active' to="/">Home</NavLink>
+
+    <NavLink activeStyle={{color: 'green'}} to='/about'>About</NavLink>
+
+    {/* Другой вариант задания ссылки */}
+    {/* <Link to={{pathname: '/about'}}>About</Link> */}
+
+    <NavLink isActive={isActiveFunc} activeClassName='active' to="/contact">Contact</NavLink>
+  </nav>
+);
+
+const App = () => (
+  <Router>
+    <div>
+      <Links/>
+
+      <Route exact path='/' component={Home}/>
+      <Route path='/about' render={() => <h1>About</h1>}/>
+      <Route path='/contact' render={() => <h1>Contact</h1>}/>
+    </div>
+  </Router>
+);
 
 export default App;
