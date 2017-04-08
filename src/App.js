801cc8b50e51effdc06cc4429eae0512e9e4eaf8
Sergey Bogdanov <bgrodthai@gmail.com>
2017-04-08 15:18:41 +0300

    Add nested routes

--- src/App.js
+++ src/App.js
@@ -1,39 +1,32 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import './App.css';
 
-const Links = () => (
-  <nav>
-    <Link to='/'>Home</Link>
-    <Link to='/about'>About</Link>
-  </nav>
-);
+const Home = () => <h1>Home</h1>;
+const Menu = () => (
+  <div>
+    <h1>Menu</h1>
 
-// props destruction
-const Header = ({match}) => (
-  <div className="header">
+    <Link to="/menu/food">Food</Link>
+    <Link to="/menu/drinks">Drinks</Link>
+    <Link to="/menu/sides">Sides</Link>
+
     {/* route props destruction */}
-    <Route path='/:page' render={({match}) => (
-      <h1>{match.params.page} header</h1>
-    )}/>
-  </div>
-);
-
-const Content = ({match}) => (
-  <div className="content">
-    <Route path='/:page' render={({match}) => (
-      <p>{match.params.page} content</p>
-    )}/>
+    <Route path="/menu/:section" render={({match}) => (
+      <h2>{match.params.section}</h2>
+    )} />
   </div>
 );
 
 const App = (props) => (
   <Router>
     <div>
-      <Links />
-      <Header/>
-      <Content/>
+      <Link to='/'>Home</Link>
+      <Link to='/menu'>Menu</Link>
+
+      <Route exact path='/' component={Home} />
+      <Route path='/menu' component={Menu} />
     </div>
   </Router>
 );
