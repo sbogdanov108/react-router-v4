726895a0e58dbb540e3abaece812c38566e4df05
Sergey Bogdanov <bgrodthai@gmail.com>
2017-04-08 15:29:47 +0300

    Add redirect

--- src/App.js
+++ src/App.js
@@ -1,32 +1,42 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
 
 import './App.css';
 
-const Home = () => <h1>Home</h1>;
-const Menu = () => (
-  <div>
-    <h1>Menu</h1>
+const loggedIn = true;
 
-    <Link to="/menu/food">Food</Link>
-    <Link to="/menu/drinks">Drinks</Link>
-    <Link to="/menu/sides">Sides</Link>
-
-    {/* route props destruction */}
-    <Route path="/menu/:section" render={({match}) => (
-      <h2>{match.params.section}</h2>
-    )} />
-  </div>
+const Links = () => (
+  <nav>
+    <Link to='/'>Home</Link>
+    <Link to='/old/123'>Old</Link>
+    <Link to='/new/456'>New</Link>
+    <Link to='/protected'>Protected</Link>
+  </nav>
 );
 
 const App = (props) => (
   <Router>
     <div>
-      <Link to='/'>Home</Link>
-      <Link to='/menu'>Menu</Link>
+      <Links />
 
-      <Route exact path='/' component={Home} />
-      <Route path='/menu' component={Menu} />
+      <Route exact path='/' render={() => (<h1>Home</h1>)} />
+
+      {/* route props destruction */}
+      <Route exact path='/new/:str' render={({match}) => (
+        <h1>New: {match.params.str}</h1>
+      )} />
+
+      {/* Редирект с /old/123 на /new/456 с передачей old параметра 123 */}
+      <Route path='/old/:str' render={({match}) => (
+        <Redirect to={`/new/${match.params.str}`} />
+      )} />
+
+      {/* Простая защита роута. Происходит редирект, например, при отсутствии залогиненного юзера */}
+      <Route path='/protected' render={() => (
+        loggedIn
+          ? <h1>This is protected page</h1>
+          : <Redirect to={'/new/login'} />
+      )} />
     </div>
   </Router>
 );
