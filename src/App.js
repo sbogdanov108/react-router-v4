dd63ac5fc6934f1a2c09c437e7dbca480f769a56
Sergey Bogdanov <bgrodthai@gmail.com>
2017-04-08 15:38:13 +0300

    Add prompt

--- src/App.js
+++ src/App.js
@@ -1,42 +1,38 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link, Redirect} from 'react-router-dom';
+import {BrowserRouter as Router, Route, Link, Prompt} from 'react-router-dom';
 
 import './App.css';
 
-const loggedIn = true;
+const Home = () => <h1>Home</h1>;
 
-const Links = () => (
-  <nav>
-    <Link to='/'>Home</Link>
-    <Link to='/old/123'>Old</Link>
-    <Link to='/new/456'>New</Link>
-    <Link to='/protected'>Protected</Link>
-  </nav>
-);
+class Form extends React.Component{
+  state = {dirty: false}; // состояние формы - нет изменений
+
+  setDirty = () => this.setState({dirty: true});
+
+  render() {
+    return (
+      <div>
+        <h1>Form</h1>
+
+        <input type="text" onInput={this.setDirty}/>
+
+        <Prompt
+          when={this.state.dirty}
+          message="Данные будут потеряны!" />
+      </div>
+    )
+  }
+}
 
 const App = (props) => (
   <Router>
     <div>
-      <Links />
+      <Link to='/'>Home</Link>
+      <Link to='/form'>Form</Link>
 
-      <Route exact path='/' render={() => (<h1>Home</h1>)} />
-
-      {/* route props destruction */}
-      <Route exact path='/new/:str' render={({match}) => (
-        <h1>New: {match.params.str}</h1>
-      )} />
-
-      {/* Редирект с /old/123 на /new/456 с передачей old параметра 123 */}
-      <Route path='/old/:str' render={({match}) => (
-        <Redirect to={`/new/${match.params.str}`} />
-      )} />
-
-      {/* Простая защита роута. Происходит редирект, например, при отсутствии залогиненного юзера */}
-      <Route path='/protected' render={() => (
-        loggedIn
-          ? <h1>This is protected page</h1>
-          : <Redirect to={'/new/login'} />
-      )} />
+      <Route exact path='/' component={Home} />
+      <Route path='/form' component={Form} />
     </div>
   </Router>
 );
