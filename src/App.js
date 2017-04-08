2ba83fb48e58fda22e6e9f5f8696b17100f8fb5a
Sergey Bogdanov <bgrodthai@gmail.com>
2017-04-08 15:43:18 +0300

    Add different router types

--- src/App.js
+++ src/App.js
@@ -1,40 +1,56 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link, Prompt} from 'react-router-dom';
+import {BrowserRouter, HashRouter, MemoryRouter, StaticRouter, Route, Link} from 'react-router-dom';
 
 import './App.css';
 
-const Home = () => <h1>Home</h1>;
+const LinksRoutes = () => (
+  <div>
+    <Link to='/'>Home</Link>
+    <Link to='/about'>About</Link>
 
-class Form extends React.Component{
-  state = {dirty: false}; // состояние формы - нет изменений
-
-  setDirty = () => this.setState({dirty: true});
-
-  render() {
-    return (
-      <div>
-        <h1>Form</h1>
-
-        <input type="text" onInput={this.setDirty}/>
-
-        <Prompt
-          when={this.state.dirty}
-          message="Данные будут потеряны!" />
-      </div>
-    )
-  }
-}
-
-const App = (props) => (
-  <Router>
-    <div>
-      <Link to='/'>Home</Link>
-      <Link to='/form'>Form</Link>
-
-      <Route exact path='/' component={Home} />
-      <Route path='/form' component={Form} />
-    </div>
-  </Router>
+    <Route exact path='/' render={() => <h1>Home</h1>} />
+    <Route path='/about' render={() => <h1>About</h1>} />
+  </div>
 );
 
-export default App;
+// Принудительное обновление страницы
+const forceRefresh = () => {
+  console.log(new Date());
+  return false;
+};
+
+const BrowserRouterApp = () => (
+  // HTML5 History
+  <BrowserRouter forceRefresh={forceRefresh()}>
+    <LinksRoutes />
+  </BrowserRouter>
+);
+
+const HashRouterApp = () => (
+  // Hash History
+  <HashRouter hashType='slash'>
+    <LinksRoutes />
+  </HashRouter>
+);
+
+const MemoryRouterApp = () => (
+  // Memory History - for testing purpose
+  <MemoryRouter
+    initialEntries={['/', '/about']}
+    initialIndex={1}
+  >
+    <LinksRoutes />
+  </MemoryRouter>
+);
+
+const StaticRouterApp = () => (
+  // Static - for server-side rendering
+  <StaticRouter location='/about' context={{}}>
+    <LinksRoutes />
+  </StaticRouter>
+);
+
+//export default StaticRouterApp;
+//export default MemoryRouterApp;
+export default HashRouterApp;
+//export default BrowserRouterApp;
